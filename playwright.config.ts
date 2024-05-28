de2a5f2bd72bdfa7f54a3c9e5bd00e7e1b6d7b29
Chris McNeill <chris.mcneill_pa@hra.nhs.uk>
2024-05-28 08:26:02 +0100

    reverting config for pr

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { PlaywrightTestConfig, webkit, devices } from '@playwright/test';
+import { PlaywrightTestConfig, devices } from '@playwright/test';
 import { defineBddConfig, cucumberReporter } from 'playwright-bdd';
 import 'dotenv/config';
 
@@ -12,9 +12,7 @@ if (`${process.env.BROWSER}` == 'safari') {
   browser = devices['Desktop Firefox'];
   platform = 'desktop';
 } else if (`${process.env.BROWSER}` == 'ios') {
-  browser = webkit.name(); //this works for ios, check other device configs, ie set isMobile to true. 
-  // if no joy use for others and set relative user agents above
-  // browser = devices['iPhone 14'];
+  browser = devices['iPhone 14'];
   platform = 'mobile';
 } else if (`${process.env.BROWSER}` == 'android') {
   browser = devices['Galaxy S8'];
@@ -34,27 +32,12 @@ const config: PlaywrightTestConfig = {
   globalTeardown: 'src/hooks/GlobalTeardown.ts',
   timeout: 30000,
   workers: 1, // to enforce serial execution
-  // retries: 2,
+  retries: 2,
   use: {
-    // viewport: devices['iPhone 14'].viewport,
-    userAgent: devices['iPhone 14'].userAgent,
-    isMobile: devices['iPhone 14'].isMobile,
-    hasTouch: devices['iPhone 14'].hasTouch,
-    defaultBrowserType: devices['iPhone 14'].defaultBrowserType,
-    // defaultBrowserType: 'webkit',
-    deviceScaleFactor: devices['iPhone 14'].deviceScaleFactor,
-    // ...browser,
-    // browserName: browser,
-    // userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Mobile/15E148 Safari/604.1",
-    // userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/16.0 Mobile/14E304 Safari/602.1",
-    // ...browser,
-    viewport: {
-      width: 320,
-      height: 568
-    },
+    ...browser,
     trace: 'on',
     baseURL: `${process.env.BASE_URL}`,
-    screenshot: 'on'
+    screenshot: 'only-on-failure'
   },
   projects: [
     {
@@ -67,7 +50,6 @@ const config: PlaywrightTestConfig = {
         outputDir: 'generated-feature-files/auth-setup',
       }),
       use: {
-        // ...browser,
         headless: true,
         launchOptions: {
           slowMo: 0,
@@ -84,7 +66,6 @@ const config: PlaywrightTestConfig = {
         outputDir: 'generated-feature-files/old-iras',
       }),
       use: {
-        // ...browser,
         headless: true,
         launchOptions: {
           slowMo: 0,
