03acad2ec4606e1fb95aaea1d8b7fd4e3abc8ae2
Chris McNeill <chris.mcneill_pa@hra.nhs.uk>
2024-05-28 01:54:35 +0100

    adding different config for ios testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,9 +12,9 @@ if (`${process.env.BROWSER}` == 'safari') {
   browser = devices['Desktop Firefox'];
   platform = 'desktop';
 } else if (`${process.env.BROWSER}` == 'ios') {
-  // browser = webkit.name(); //this works for ios, check other device configs, ie set isMobile to true. 
+  browser = webkit.name(); //this works for ios, check other device configs, ie set isMobile to true. 
   // if no joy use for others and set relative user agents above
-  browser = devices['iPhone 14'];
+  // browser = devices['iPhone 14'];
   platform = 'mobile';
 } else if (`${process.env.BROWSER}` == 'android') {
   browser = devices['Galaxy S8'];
@@ -44,7 +44,7 @@ const config: PlaywrightTestConfig = {
     // defaultBrowserType: 'webkit',
     deviceScaleFactor: devices['iPhone 14'].deviceScaleFactor,
     // ...browser,
-    browserName: 'webkit',
+    browserName: browser,
     // userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Mobile/15E148 Safari/604.1",
     // userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/16.0 Mobile/14E304 Safari/602.1",
     // ...browser,
