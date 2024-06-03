cea3cf66188372c030102d0f74a9a54ff96f3dcb
alfred.dcruz <alfred.dcruz@hra.nhs.uk>
2024-06-03 09:36:02 +0100

    Encryption generator utility added

---
+++ src/utils/Encrypt.js
@@ -0,0 +1,9 @@
+const CryptoJS = require("crypto-js");
+
+// Run this utility to generate an encrypted value. Replace '<Add secret key here>' with the secret key.
+
+var encrypted = CryptoJS.AES.encrypt("password", "<Add secret key here>");
+var decrypted = CryptoJS.AES.decrypt(encrypted, "<Add secret key here>");
+
+console.log("Encrypted Text: " + encrypted.toString());
+console.log("Decrypted Text: " + decrypted.toString(CryptoJS.enc.Utf8));
