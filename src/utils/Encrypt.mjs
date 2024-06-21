c381cc355f422e5492399195fd52679a8faf6397
Chris McNeill <chris.mcneill_pa@hra.nhs.uk>
2024-06-21 19:33:32 +0100

    testing pre-commit hook

--- src/utils/Encrypt.mjs
+++ src/utils/Encrypt.mjs
@@ -1,30 +1,30 @@
-import { createCipheriv, createDecipheriv } from 'crypto';
+import { createCipheriv, createDecipheriv } from 'crypto'
 //Investigate use of quote enforcement on linter, to use ' but doesn't restrict the backtick (ctrl+z)
 
 // Run this utility to generate an encrypted value. Replace '<Add secret key here which is 32 character long>' with the secret key. This should be 32 characters long.
 // Replace "sensitive-data" with user password
 
-const encryption_key = "SuperSecretKeySuperSecretKey2024";
-const textToEncrypt = "PROST3jerocrugaml";
+const encryption_key = '<Add secret key here which is 32 character long>'
+const textToEncrypt = '<sensitive-data>'
 
-function encrypt(text){
-  const cipher = createCipheriv('aes-256-cbc',Buffer.from(encryption_key), Buffer.alloc(16));
-  var crypted = cipher.update(text, 'utf8', 'hex');
-  crypted += cipher.final('hex');
-  return crypted;
+function encrypt(text) {
+  const cipher = createCipheriv('aes-256-cbc', Buffer.from(encryption_key), Buffer.alloc(16))
+  var crypted = cipher.update(text, 'utf8', 'hex')
+  crypted += cipher.final('hex')
+  return crypted
 }
 
-function decrypt(text){
-  const decipher = createDecipheriv('aes-256-cbc',Buffer.from(encryption_key), Buffer.alloc(16));
-  let dec = decipher.update(text, 'hex', 'utf8');
-  dec += decipher.final('utf8');
-  return dec;
+function decrypt(text) {
+  const decipher = createDecipheriv('aes-256-cbc', Buffer.from(encryption_key), Buffer.alloc(16))
+  let dec = decipher.update(text, 'hex', 'utf8')
+  dec += decipher.final('utf8')
+  return dec
 }
- 
-var encrypted = encrypt(textToEncrypt);
+
+var encrypted = encrypt(textToEncrypt)
 
 // Encrypting
-console.log("Encrypted Value: " + encrypted);
- 
+console.log('Encrypted Value: ' + encrypted)
+
 // Decrypting
-console.log("Decrypted Value: " + decrypt(encrypted));
+console.log('Decrypted Value: ' + decrypt(encrypted))
