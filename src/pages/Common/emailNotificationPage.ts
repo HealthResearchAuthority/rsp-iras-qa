445329c08584b122ef4999ecefec302d158d02c4
alfred.dcruz <alfred.dcruz@hra.nhs.uk>
2025-01-10 12:32:06 +0000

    RSP-2094 - Updated locators, feature file and step definition

--- src/pages/Common/emailNotificationPage.ts
+++ src/pages/Common/emailNotificationPage.ts
@@ -3,32 +3,34 @@ import * as emailNotificationTestData from '../../resources/test_data/common/ema
 
 //Declare Page Objects
 export default class EmailNotificationPage {
-  readonly page: Page;
+  emailPage: Page;
   readonly emailNotificationTestData: typeof emailNotificationTestData;
-  emailDomainName: string;
   readonly userNameMicrosoftTextBox: string;
   readonly passwordMicrosoftTextBox: string;
   readonly submitMicrosoftButton: string;
+  readonly filterMailButton: string;
+  readonly unreadMenuFromfilterButton: string;
   readonly emailSubjectMicrosoftListLabel: string;
   readonly emailMarkAsReadMicrosoftButton: string;
   readonly emailFromHeaderMicrosoftLabel: string;
   readonly emailSubjectHeaderMicrosoftLabel: string;
   readonly emailBodyMicrosoftLabel: string;
-  emailPage: Page;
 
   //Initialize Page Objects
-  constructor(page: Page) {
-    this.page = page;
+  constructor(emailPage: Page) {
+    this.emailPage = emailPage;
     this.emailNotificationTestData = emailNotificationTestData;
 
     //Locators
     this.userNameMicrosoftTextBox = 'input[type="email"]';
     this.passwordMicrosoftTextBox = 'input[type="password"]';
     this.submitMicrosoftButton = 'input[type="submit"]';
-    this.emailSubjectMicrosoftListLabel = '//div[contains(@aria-label,"Unread")]/descendant::span[text()="%s"]';
-    this.emailMarkAsReadMicrosoftButton = '//span[text()="Read / Unread"]';
-    this.emailFromHeaderMicrosoftLabel = '//span[contains(@aria-label,"From")]/descendant::span';
-    this.emailSubjectHeaderMicrosoftLabel = 'div[id="ConversationReadingPaneContainer"] div[role="heading"] span';
+    this.filterMailButton = 'button[id="mailListFilterMenu"]';
+    this.unreadMenuFromfilterButton = 'span[class="UagSo"]';
+    this.emailSubjectMicrosoftListLabel = 'span';
+    this.emailMarkAsReadMicrosoftButton = 'span';
+    this.emailFromHeaderMicrosoftLabel = 'span[class="OZZZK"]';
+    this.emailSubjectHeaderMicrosoftLabel = 'span[class="JdFsz"]';
     this.emailBodyMicrosoftLabel = 'div[aria-label="Message body"]';
   }
 }
--- src/steps/Common/EmailNotificationSteps.ts
+++ src/steps/Common/EmailNotificationSteps.ts
@@ -4,50 +4,63 @@ import { chromium, expect } from '@playwright/test';
 const { Then } = createBdd(test);
 import { getDecryptedValue } from '../../utils/UtilFunctions';
 
-Then('I login to the {string} email application', async ({ emailNotificationPage }, datasetName: string) => {
-  emailNotificationPage.emailDomainName = datasetName.replace(/_.*/, '');
-  const emailDataSetName = datasetName.replace(/[^_]*_/, '');
-  const datasetParent = emailNotificationPage.emailNotificationTestData[emailNotificationPage.emailDomainName];
-  const dataset = datasetParent[emailDataSetName];
-  const browser = await chromium.launch();
-  emailNotificationPage.emailPage = await browser.newPage();
-  await emailNotificationPage.emailPage.goto(dataset.url);
-  if (emailNotificationPage.emailDomainName.toLowerCase() === 'microsoft') {
-    await emailNotificationPage.emailPage
-      .locator(emailNotificationPage.userNameMicrosoftTextBox)
-      .fill(dataset.username);
-    await emailNotificationPage.emailPage.locator(emailNotificationPage.submitMicrosoftButton).click();
-    await emailNotificationPage.emailPage
-      .locator(emailNotificationPage.passwordMicrosoftTextBox)
-      .fill(getDecryptedValue(eval(dataset.password)));
-    await emailNotificationPage.emailPage.locator(emailNotificationPage.submitMicrosoftButton).click();
-    await emailNotificationPage.emailPage.locator(emailNotificationPage.submitMicrosoftButton).click();
+Then(
+  'I login to the {string} email application with {string} credentials',
+  async ({ emailNotificationPage }, domainName: string, datasetName: string) => {
+    const datasetParent = emailNotificationPage.emailNotificationTestData[domainName];
+    const dataset = datasetParent[datasetName];
+    const browser = await chromium.launch();
+    const context = await browser.newContext({ javaScriptEnabled: true });
+    emailNotificationPage.emailPage = await context.newPage();
+    await emailNotificationPage.emailPage.goto(dataset.url);
+    if (domainName.toLowerCase() === 'microsoft') {
+      await emailNotificationPage.emailPage
+        .locator(emailNotificationPage.userNameMicrosoftTextBox)
+        .fill(dataset.username);
+      await emailNotificationPage.emailPage.locator(emailNotificationPage.submitMicrosoftButton).click();
+      await emailNotificationPage.emailPage
+        .locator(emailNotificationPage.passwordMicrosoftTextBox)
+        .fill(getDecryptedValue(eval(dataset.password)));
+      await emailNotificationPage.emailPage.locator(emailNotificationPage.submitMicrosoftButton).click();
+      await emailNotificationPage.emailPage.locator(emailNotificationPage.submitMicrosoftButton).click();
+    }
   }
-});
+);
 
-Then('I validate the {string} in email notification', async ({ emailNotificationPage }, datasetName: string) => {
-  const dataset = emailNotificationPage.emailNotificationTestData[datasetName];
-  const fromAddressExpected = dataset.from;
-  const emailSubjectExpected = dataset.subject;
-  const emailBodyExpected = dataset.body;
-  if (emailNotificationPage.emailDomainName.toLowerCase() === 'microsoft') {
-    const emailSubjectLocatorString = emailNotificationPage.emailSubjectMicrosoftListLabel.replace(
-      '%s',
-      dataset.subject
-    );
-    await emailNotificationPage.emailPage
-      .locator(emailSubjectLocatorString)
-      .first()
-      .click({ timeout: dataset.mail_timeout });
-    await emailNotificationPage.emailPage.click(emailNotificationPage.emailMarkAsReadMicrosoftButton);
-    await expect(
-      emailNotificationPage.emailPage.locator(emailNotificationPage.emailFromHeaderMicrosoftLabel).first()
-    ).toContainText(fromAddressExpected);
-    await expect(
-      emailNotificationPage.emailPage.locator(emailNotificationPage.emailSubjectHeaderMicrosoftLabel).first()
-    ).toContainText(emailSubjectExpected);
-    expect(
-      await emailNotificationPage.emailPage.locator(emailNotificationPage.emailBodyMicrosoftLabel).first().textContent()
-    ).toContain(emailBodyExpected);
+Then(
+  'I validate the {string} on the {string} email',
+  async ({ emailNotificationPage }, datasetName: string, domainName: string) => {
+    const dataset = emailNotificationPage.emailNotificationTestData[datasetName];
+    const fromAddressExpected = dataset.from;
+    const emailSubjectExpected = dataset.subject;
+    const emailBodyExpected = dataset.body;
+    if (domainName.toLowerCase() === 'microsoft') {
+      await emailNotificationPage.emailPage.locator(emailNotificationPage.filterMailButton).click();
+      await emailNotificationPage.emailPage
+        .locator(emailNotificationPage.unreadMenuFromfilterButton)
+        .getByText('Unread')
+        .click();
+      await emailNotificationPage.emailPage
+        .locator(emailNotificationPage.emailSubjectMicrosoftListLabel)
+        .getByText(dataset.subject)
+        .first()
+        .click({ timeout: dataset.mail_timeout });
+      await expect(
+        emailNotificationPage.emailPage.locator(emailNotificationPage.emailFromHeaderMicrosoftLabel).first()
+      ).toContainText(fromAddressExpected);
+      await expect(
+        emailNotificationPage.emailPage.locator(emailNotificationPage.emailSubjectHeaderMicrosoftLabel).first()
+      ).toContainText(emailSubjectExpected);
+      expect(
+        await emailNotificationPage.emailPage
+          .locator(emailNotificationPage.emailBodyMicrosoftLabel)
+          .first()
+          .textContent()
+      ).toContain(emailBodyExpected);
+      await emailNotificationPage.emailPage
+        .locator(emailNotificationPage.emailMarkAsReadMicrosoftButton)
+        .getByText('Read / Unread')
+        .click();
+    }
   }
-});
+);
