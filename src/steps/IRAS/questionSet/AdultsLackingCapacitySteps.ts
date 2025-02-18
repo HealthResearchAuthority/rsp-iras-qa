cd8bfdfcb8808f2abafdabe806f5c22dbfbf6099
Chris McNeill <chris.mcneill_pa@hra.nhs.uk>
2025-02-18 10:17:31 +0000

    updating qset steps with with latest data entry method

--- src/steps/IRAS/questionSet/AdultsLackingCapacitySteps.ts
+++ src/steps/IRAS/questionSet/AdultsLackingCapacitySteps.ts
@@ -6,7 +6,7 @@ const { Then } = createBdd(test);
 Then(
   'I fill the {string} section in the adults lacking capacity page with {string}',
   async ({ adultsLackingCapacityPage, commonItemsPage }, sectionName: string, datasetName: string) => {
-    const dataset = await adultsLackingCapacityPage.getTestDataForALC(sectionName, datasetName);
+    const dataset = adultsLackingCapacityPage.adultsLackingCapacityPageTestData[sectionName][datasetName];
     for (const key in dataset) {
       if (Object.prototype.hasOwnProperty.call(dataset, key)) {
         await commonItemsPage.fillUIComponent(dataset, key, adultsLackingCapacityPage);
