cf39dcf02c0767f6a73df3df0f5037729f77837e
Tiji Manikombel Jose <tiji.manikombel-jose@hra.nhs.uk>
2025-04-14 19:59:58 +0100

    avoiding code duplication

--- src/steps/IRAS/reviewResearch/userAdministration/manageReviewBodies/AuditHistoryReviewBodySteps.ts
+++ src/steps/IRAS/reviewResearch/userAdministration/manageReviewBodies/AuditHistoryReviewBodySteps.ts
@@ -92,6 +92,22 @@ Then(
       auditHistoryReviewBodyPage.auditHistoryReviewBodyPageTestData.Review_Body_Audit_History_Page[datasetName];
     const auditLog = await auditHistoryReviewBodyPage.getAuditLog();
     const timeExpected = await auditHistoryReviewBodyPage.getUpdatedTime();
+    const timeIndex = parseInt(
+      auditHistoryReviewBodyPage.auditHistoryReviewBodyPageTestData.Review_Body_Audit_History_Page.time_index,
+      10
+    );
+    const descrIndex = parseInt(
+      auditHistoryReviewBodyPage.auditHistoryReviewBodyPageTestData.Review_Body_Audit_History_Page.description_index,
+      10
+    );
+    const emailIndex = parseInt(
+      auditHistoryReviewBodyPage.auditHistoryReviewBodyPageTestData.Review_Body_Audit_History_Page.admin_email_index,
+      10
+    );
+    const rowIndex = parseInt(
+      auditHistoryReviewBodyPage.auditHistoryReviewBodyPageTestData.Review_Body_Audit_History_Page.index,
+      10
+    );
     let valuePrevious: string = '';
     let valueCurrent: string = '';
     if (fieldName == 'Organisation_Name') {
@@ -112,13 +128,10 @@ Then(
       valueCurrent = await reviewBodyProfilePage.getNewDescription();
     }
     if (valuePrevious !== valueCurrent) {
-      const eventDescriptionPart1 = dataset.event_description_text;
-      const eventDescriptionPart2 = valuePrevious;
-      const eventDescriptionPart3 = ' to ' + valueCurrent;
-      const eventDescriptionExpectedValue = eventDescriptionPart1 + eventDescriptionPart2 + eventDescriptionPart3;
-      expect(auditLog[0][0]).toBe(timeExpected);
-      expect(auditLog[1][0]).toBe(eventDescriptionExpectedValue);
-      expect(auditLog[2][0]).toBe(dataset.system_admin_email_text);
+      const eventDescriptionExpectedValue = dataset.event_description_text + valuePrevious + ' to ' + valueCurrent;
+      expect(auditLog[timeIndex][rowIndex]).toBe(timeExpected);
+      expect(auditLog[descrIndex][rowIndex]).toBe(eventDescriptionExpectedValue);
+      expect(auditLog[emailIndex][rowIndex]).toBe(dataset.system_admin_email_text);
     }
   }
 );
@@ -126,79 +139,66 @@ Then(
 Then(
   'I can see the audit history for all the fields edited event with {string}',
   async ({ auditHistoryReviewBodyPage, reviewBodyProfilePage }, datasetName: string) => {
-    // defect is there when the previous description was empty
+    // defect: when the previous description was empty
     const dataset =
       auditHistoryReviewBodyPage.auditHistoryReviewBodyPageTestData.Review_Body_Audit_History_Page[datasetName];
     const auditLog = await auditHistoryReviewBodyPage.getAuditLog();
     const timeExpected = await auditHistoryReviewBodyPage.getUpdatedTime();
     const descriptionPrevious = await reviewBodyProfilePage.getDescription();
     const descriptionCurrent = await reviewBodyProfilePage.getNewDescription();
-    const descriptionEventDescriptionPart1 = dataset.description_event_description_text;
-    const descriptionEventDescriptionPart2 = descriptionPrevious;
-    const descriptionEventDescriptionPart3 = ' to ' + descriptionCurrent;
     const descriptionEventDescriptionExpectedValue =
-      descriptionEventDescriptionPart1 + descriptionEventDescriptionPart2 + descriptionEventDescriptionPart3;
+      dataset.description_event_description_text + descriptionPrevious + ' to ' + descriptionCurrent;
     const emailPrevious = await reviewBodyProfilePage.getEmail();
     const emailCurrent = await reviewBodyProfilePage.getNewEmail();
-    const emailEventDescriptionPart1 = dataset.email_address_event_description_text;
-    const emailEventDescriptionPart2 = emailPrevious;
-    const emailEventDescriptionPart3 = ' to ' + emailCurrent;
     const emailEventDescriptionExpectedValue =
-      emailEventDescriptionPart1 + emailEventDescriptionPart2 + emailEventDescriptionPart3;
+      dataset.email_address_event_description_text + emailPrevious + ' to ' + emailCurrent;
     const countryNamesPrevious: string = (await reviewBodyProfilePage.getCountries()).join(', ');
     const countryNamesCurrent = (await reviewBodyProfilePage.getNewCountries()).join(', ');
-    const CountryEventDescriptionPart1 = dataset.country_event_description_text;
-    const countryEventDescriptionPart2 = countryNamesPrevious;
-    const countryEventDescriptionPart3 = ' to ' + countryNamesCurrent;
     const countryEventDescriptionExpectedValue =
-      CountryEventDescriptionPart1 + countryEventDescriptionPart2 + countryEventDescriptionPart3;
+      dataset.country_event_description_text + countryNamesPrevious + ' to ' + countryNamesCurrent;
     const organisationNamePrevious = await reviewBodyProfilePage.getOrgName();
     const organisationNameCurrent = await reviewBodyProfilePage.getNewOrgName();
-    const orgNameEventDescriptionPart1 = dataset.organisation_name_event_description_text;
-    const orgNameEventDescriptionPart2 = organisationNamePrevious;
-    const orgNameEventDescriptionPart3 = ' to ' + organisationNameCurrent;
     const orgNameEventDescriptionExpectedValue =
-      orgNameEventDescriptionPart1 + orgNameEventDescriptionPart2 + orgNameEventDescriptionPart3;
+      dataset.organisation_name_event_description_text + organisationNamePrevious + ' to ' + organisationNameCurrent;
     await expect(auditHistoryReviewBodyPage.page_heading).toHaveText(
       auditHistoryReviewBodyPage.auditHistoryReviewBodyPageTestData.Review_Body_Audit_History_Page.page_heading +
         organisationNameCurrent
     );
-    if (countryNamesPrevious !== countryNamesCurrent) {
-      expect(auditLog[0][3]).toBe(timeExpected);
-      expect(auditLog[1][3]).toBe(countryEventDescriptionExpectedValue);
-      expect(auditLog[2][3]).toBe(dataset.system_admin_email_text);
-      expect(auditLog[0][2]).toBe(timeExpected);
-
-      if (descriptionPrevious !== '') {
-        expect(auditLog[1][2]).toBe(descriptionEventDescriptionExpectedValue);
-      } else {
-        const descriptionEventDescriptionExpectedValue =
-          descriptionEventDescriptionPart1 + '(null)' + descriptionEventDescriptionPart3;
-        expect(auditLog[1][2]).toBe(descriptionEventDescriptionExpectedValue);
-      }
-      expect(auditLog[2][2]).toBe(dataset.system_admin_email_text);
-      expect(auditLog[0][1]).toBe(timeExpected);
-      expect(auditLog[1][1]).toBe(emailEventDescriptionExpectedValue);
-      expect(auditLog[2][1]).toBe(dataset.system_admin_email_text);
-      expect(auditLog[0][0]).toBe(timeExpected);
-      expect(auditLog[1][0]).toBe(orgNameEventDescriptionExpectedValue);
-      expect(auditLog[2][0]).toBe(dataset.system_admin_email_text);
+    const organisationIndex = parseInt(dataset.organisation_index, 10);
+    const emailIndex = parseInt(dataset.email_index, 10);
+    const descriptionIndex = parseInt(dataset.description_index, 10);
+    const countryIndex = parseInt(dataset.country_index, 10);
+    const timeIndex = parseInt(
+      auditHistoryReviewBodyPage.auditHistoryReviewBodyPageTestData.Review_Body_Audit_History_Page.time_index,
+      10
+    );
+    const descrIndex = parseInt(
+      auditHistoryReviewBodyPage.auditHistoryReviewBodyPageTestData.Review_Body_Audit_History_Page.description_index,
+      10
+    );
+    const adminEmailIndex = parseInt(
+      auditHistoryReviewBodyPage.auditHistoryReviewBodyPageTestData.Review_Body_Audit_History_Page.admin_email_index,
+      10
+    );
+    expect(auditLog[timeIndex][organisationIndex]).toBe(timeExpected);
+    expect(auditLog[descrIndex][organisationIndex]).toBe(orgNameEventDescriptionExpectedValue);
+    expect(auditLog[adminEmailIndex][organisationIndex]).toBe(dataset.system_admin_email_text);
+    expect(auditLog[timeIndex][emailIndex]).toBe(timeExpected);
+    expect(auditLog[descrIndex][emailIndex]).toBe(emailEventDescriptionExpectedValue);
+    expect(auditLog[adminEmailIndex][emailIndex]).toBe(dataset.system_admin_email_text);
+    expect(auditLog[timeIndex][descriptionIndex]).toBe(timeExpected);
+    if (descriptionPrevious !== '') {
+      expect(auditLog[descrIndex][descriptionIndex]).toBe(descriptionEventDescriptionExpectedValue);
     } else {
-      expect(auditLog[0][2]).toBe(timeExpected);
-      if (descriptionPrevious !== '') {
-        expect(auditLog[1][2]).toBe(descriptionEventDescriptionExpectedValue);
-      } else {
-        const descriptionEventDescriptionExpectedValue =
-          descriptionEventDescriptionPart1 + '(null)' + descriptionEventDescriptionPart3;
-        expect(auditLog[1][2]).toBe(descriptionEventDescriptionExpectedValue);
-      }
-      expect(auditLog[2][2]).toBe(dataset.system_admin_email_text);
-      expect(auditLog[0][1]).toBe(timeExpected);
-      expect(auditLog[1][1]).toBe(emailEventDescriptionExpectedValue);
-      expect(auditLog[2][1]).toBe(dataset.system_admin_email_text);
-      expect(auditLog[0][0]).toBe(timeExpected);
-      expect(auditLog[1][0]).toBe(orgNameEventDescriptionExpectedValue);
-      expect(auditLog[2][0]).toBe(dataset.system_admin_email_text);
+      const descriptionEventDescriptionExpectedValue =
+        dataset.description_event_description_text + '(null)' + ' to ' + descriptionCurrent;
+      expect(auditLog[descrIndex][descriptionIndex]).toBe(descriptionEventDescriptionExpectedValue);
+    }
+    expect(auditLog[adminEmailIndex][descriptionIndex]).toBe(dataset.system_admin_email_text);
+    if (countryNamesPrevious !== countryNamesCurrent) {
+      expect(auditLog[timeIndex][countryIndex]).toBe(timeExpected);
+      expect(auditLog[descrIndex][countryIndex]).toBe(countryEventDescriptionExpectedValue);
+      expect(auditLog[adminEmailIndex][countryIndex]).toBe(dataset.system_admin_email_text);
     }
   }
 );
