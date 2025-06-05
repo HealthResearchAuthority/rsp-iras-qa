0bb976e509cd68994037b52efcbf68fb1d1c5f6d
Chris McNeill <chris.mcneill_pa@hra.nhs.uk>
2025-06-05 20:15:47 +0100

    updating BDD check auth hook to ensure reauth is done with JS enabled without adjusting the test contexts JS state + adjusting request check so that it is valid for all user types + ensuring that correct user reauth state is used for the upcoming test after reauth

--- src/steps/Common/BddHooksSteps.ts
+++ src/steps/Common/BddHooksSteps.ts
@@ -1,6 +1,11 @@
 import { createBdd } from 'playwright-bdd';
 import { test } from '../../hooks/CustomFixtures';
-import { getTicketReferenceTags } from '../../utils/UtilFunctions';
+import { getAuthState, getTicketReferenceTags } from '../../utils/UtilFunctions';
+import { chromium } from '@playwright/test';
+import { readFileSync } from 'fs';
+import HomePage from '../../pages/IRAS/HomePage';
+import CommonItemsPage from '../../pages/Common/CommonItemsPage';
+import LoginPage from '../../pages/Common/LoginPage';
 
 const { AfterStep, BeforeScenario } = createBdd(test);
 
@@ -25,21 +30,49 @@ BeforeScenario(
 );
 
 BeforeScenario(
-  { name: 'Check that current auth state has not expired', tags: '@Regression or @SystemTest' },
-  async function ({ commonItemsPage, loginPage, homePage }) {
-    if (
-      (await commonItemsPage.page.request.get('application/welcome', { maxRedirects: 0 }).then((response) => {
-        return response.status();
-      })) != 200
-    ) {
+  { name: 'Check that current auth state has not expired', tags: '@Regression or @SystemTest and not @NoAuth' },
+  async function ({ commonItemsPage, $tags }) {
+    await commonItemsPage.page.context().clearCookies();
+
+    const authStateIsValid = await commonItemsPage.page.request.get('', { maxRedirects: 0 }).then(async (response) => {
+      return await response.text().then((includes) => {
+        return includes.includes('My Account homepage');
+      });
+    });
+
+    if (!authStateIsValid) {
       console.info('Current auth states have expired!\nReauthenticating test users before continuing test execution');
+      const jsBrowser = await chromium.launch();
+      const jsContext = await jsBrowser.newContext({ javaScriptEnabled: true });
+      const jsPage = await jsContext.newPage();
+      const jsCommonItemsPage = new CommonItemsPage(jsPage);
+      const jsHomePage = new HomePage(jsPage);
+      const jsLoginPage = new LoginPage(jsPage);
+
+      ////////FOR TIJI - LOOP BEGINS
+      await jsCommonItemsPage.page.context().clearCookies();
+      await jsHomePage.goto();
+      await jsHomePage.loginBtn.click();
+      await jsLoginPage.assertOnLoginPage();
+      await jsLoginPage.loginWithUserCreds('Admin_User');
+      await jsHomePage.assertOnHomePage();
+      await jsCommonItemsPage.storeAuthState('Admin_User');
+      await jsContext.close();
+      await jsBrowser.close();
+      ///////////////FOR TIJI - LOOP LOOP ENDS
+
+      //////////FOR TIJI - LOOP BEGINS - Add if statements for each new user
+      let newCookiesCurrentUser: any;
+      if ($tags.includes('@adminUser')) {
+        const reauthenticatedState = JSON.parse(readFileSync(getAuthState('adminUser')).toString());
+        newCookiesCurrentUser = reauthenticatedState.cookies;
+      } else {
+        throw new Error(
+          `A Reauthenticated User State has not been set for the current test.\nPlease review and ensure that the correct user tag has been applied to this test`
+        );
+      }
       await commonItemsPage.page.context().clearCookies();
-      await homePage.goto();
-      await homePage.loginBtn.click();
-      await loginPage.assertOnLoginPage();
-      await loginPage.loginWithUserCreds('Admin_User');
-      await homePage.assertOnHomePage();
-      await commonItemsPage.storeAuthState('Admin_User');
+      await commonItemsPage.page.context().addCookies(newCookiesCurrentUser);
     }
   }
 );
