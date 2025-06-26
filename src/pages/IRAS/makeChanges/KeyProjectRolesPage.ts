9d35f14a16c69f8e41d277b24f1158495f4093bb
vijay-kumar-hra <vijayakumar.rasappan@hra.nhs.uk>
2025-06-26 09:59:01 +0100

    feature/rsp-3939 select sponsor js disabled (#56)
    * rsp-3939 - select sponsor js disabled
    
    * rsp-4017 and rsp-4033 defect changes
    
    * Updated for PR review

--- src/pages/IRAS/makeChanges/KeyProjectRolesPage.ts
+++ src/pages/IRAS/makeChanges/KeyProjectRolesPage.ts
@@ -1,10 +1,12 @@
 import { expect, Locator, Page } from '@playwright/test';
 import * as keyProjectRolesPageTestData from '../../../resources/test_data/iras/make_changes/key_project_roles_data.json';
+import * as buttonTextData from '../../../resources/test_data/common/button_text_data.json';
 
 //Declare Page Objects
 export default class KeyProjectRolesPage {
   readonly page: Page;
   readonly keyProjectRolesPageTestData: typeof keyProjectRolesPageTestData;
+  readonly buttonTextData: typeof buttonTextData;
   readonly pageHeading: Locator;
   readonly chief_investigator_header_label: Locator;
   readonly chief_investigator_email_label: Locator;
@@ -21,6 +23,11 @@ export default class KeyProjectRolesPage {
   readonly primary_sponsor_organisation_suggestion_listbox: Locator;
   readonly primary_sponsor_organisation_jsdisabled_search_button: Locator;
   readonly primary_sponsor_organisation_jsdisabled_search_results_radio_button: Locator;
+  readonly primary_sponsor_organisation_jsdisabled_result_hint_label: Locator;
+  readonly primary_sponsor_organisation_jsdisabled_search_results_labels: Locator;
+  readonly primary_sponsor_organisation_jsdisabled_narrow_down_label: Locator;
+  readonly primary_sponsor_organisation_jsdisabled_no_suggestions_label: Locator;
+  readonly primary_sponsor_organisation_jsdisabled_min_error_message: Locator;
 
   //Initialize Page Objects
   constructor(page: Page) {
@@ -44,18 +51,26 @@ export default class KeyProjectRolesPage {
     this.primary_sponsor_organisation_jsenabled_text = this.page.getByRole('combobox', {
       name: keyProjectRolesPageTestData.Label_Texts.primary_sponsor_organisation_header_label,
     });
+    this.primary_sponsor_organisation_jsdisabled_search_button = this.page.getByRole('button', {
+      name: buttonTextData.Key_Project_Roles_Page.Search,
+    });
     this.primary_sponsor_organisation_suggestion_list_labels = this.primary_sponsor_organisation_jsenabled_text
       .locator('..')
       .getByRole('option');
     this.primary_sponsor_organisation_suggestion_listbox = this.primary_sponsor_organisation_jsenabled_text
       .locator('..')
       .getByRole('listbox');
-    this.primary_sponsor_organisation_jsdisabled_search_button = this.page.getByRole('button', {
-      name: 'Search',
-    });
     this.primary_sponsor_organisation_jsdisabled_search_results_radio_button = this.page.locator(
       'input[type="radio"][name="SponsorOrgSearch.SelectedOrganisation"]'
     );
+    this.primary_sponsor_organisation_jsdisabled_result_hint_label = this.page.getByTestId(
+      'SponsorOrgSearch.SelectedOrganisation-hint'
+    );
+    this.primary_sponsor_organisation_jsdisabled_narrow_down_label = this.page.locator('.govuk-inset-text');
+    this.primary_sponsor_organisation_jsdisabled_search_results_labels = this.page
+      .locator('.govuk-radios__item')
+      .filter({ has: this.page.locator('.govuk-radios__label') });
+    this.primary_sponsor_organisation_jsdisabled_no_suggestions_label = this.page.locator('.govuk-inset-text');
   }
 
   //Page Methods
