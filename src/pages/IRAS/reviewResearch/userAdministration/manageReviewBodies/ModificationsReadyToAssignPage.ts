68c4a1437bf0894ef2c5aed8530858b81993b3ae
poornima.jayaraj <poornima.jayaraj@hra.nhs.uk>
2025-07-15 09:47:38 +0100

    Changes to 4040

--- src/pages/IRAS/reviewResearch/userAdministration/manageReviewBodies/ModificationsReadyToAssignPage.ts
+++ src/pages/IRAS/reviewResearch/userAdministration/manageReviewBodies/ModificationsReadyToAssignPage.ts
@@ -1,15 +1,80 @@
-// // import { expect, Locator, Page } from '@playwright/test';
-// // import * as modificationsReadyToAssignPageData from '../../../../../resources/test_data/iras/reviewResearch/userAdministration/manageReviewBodies/modifications_ready_to_assign.json.json';
-// import * as buttonTextData from '../../../../../resources/test_data/common/button_text_data.json';
+//import { expect, Locator, Page } from '@playwright/test';
+import * as modificationsReadyToAssignPageData from '../../../../../resources/test_data/iras/reviewResearch/receiveAmendments/modifications_ready_to_assign_page_data.json';
 
-// //Declare Page Objects
-// export default class ModificationsReadyToAssignPage {
-//   readonly page: Page;
-//   readonly modificationsReadyToAssignPageData: typeof modificationsReadyToAssignPageData;
+//Declare Page Objects
+export default class ModificationsReadyToAssignPage {
+  readonly page: Page;
+  readonly modificationsReadyToAssignPageData: typeof modificationsReadyToAssignPageData;
 
-//   readonly buttonTextData: typeof buttonTextData;
-//   readonly confirmation_panel_heading: Locator;
-//   readonly confirmation_email_note: Locator;
-//   readonly add_another_review_body_link: Locator;
-//   readonly back_manage_review_bodies_link: Locator;
-// }
+  readonly page_heading: Locator;
+  readonly page_description: Locator;
+  readonly modification_button_label: Locator;
+  readonly advanced_filter_label: Locator;
+  readonly search_button_label: Locator;
+  readonly modification_checkbox: Locator;
+  readonly short_project_title_label: Locator;
+  readonly modification_id_label: Locator;
+  readonly modification_type_label: Locator;
+  readonly date_submitted_label: Locator;
+  readonly days_since_submission_label: Locator;
+
+  //Initialize Page Objects
+  constructor(page: Page) {
+    this.page = page;
+    this.modificationsReadyToAssignPageData = modificationsReadyToAssignPageData;
+
+    //Locators
+    this.page_heading = this.page
+      .getByRole('heading')
+      .getByText(this.modificationsReadyToAssignPageData.Modifications_Ready_To_Assign_Page.page_heading, {
+        exact: true,
+      });
+    this.page_description = this.page
+      .getByRole('paragraph')
+      .getByText(this.modificationsReadyToAssignPageData.Modifications_Ready_To_Assign_Page.page_description, {
+        exact: true,
+      });
+    this.modification_button_label = this.page
+      .getByRole('paragraph')
+      .getByText(this.modificationsReadyToAssignPageData.Modifications_Ready_To_Assign_Page.modification_button_label, {
+        exact: true,
+      });
+    this.advanced_filter_label = this.page.getByRole('button', {
+      name: this.modificationsReadyToAssignPageData.Modifications_Ready_To_Assign_Page.advanced_filter_label,
+    });
+    this.search_button_label = this.page.getByText('Search');
+    this.modification_checkbox = this.page.getByRole('button', {
+      name: this.modificationsReadyToAssignPageData.Modifications_Ready_To_Assign_Page.modification_checkbox,
+    });
+    this.short_project_title_label = this.page
+      .getByRole('paragraph')
+      .getByText(this.modificationsReadyToAssignPageData.Modifications_Ready_To_Assign_Page.short_project_title_label, {
+        exact: true,
+      });
+    this.modification_id_label = this.page
+      .getByRole('paragraph')
+      .getByText(this.modificationsReadyToAssignPageData.Modifications_Ready_To_Assign_Page.modification_id_label, {
+        exact: true,
+      });
+    this.modification_type_label = this.page
+      .getByRole('paragraph')
+      .getByText(this.modificationsReadyToAssignPageData.Modifications_Ready_To_Assign_Page.modification_type_label, {
+        exact: true,
+      });
+    this.date_submitted_label = this.page
+      .getByRole('paragraph')
+      .getByText(this.modificationsReadyToAssignPageData.Modifications_Ready_To_Assign_Page.date_submitted_label, {
+        exact: true,
+      });
+    this.days_since_submission_label = this.page
+      .getByRole('paragraph')
+      .getByText(
+        this.modificationsReadyToAssignPageData.Modifications_Ready_To_Assign_Page.days_since_submission_label,
+        {
+          exact: true,
+        }
+      );
+  }
+
+  //Getters & Setters for Private Variables
+}
--- tests/features/cross-compatible/stories/systemTests/reviewResearch/userAdministration/manageReviewBodies/SysAdmin_Modification.feature
+++ tests/features/cross-compatible/stories/systemTests/reviewResearch/userAdministration/manageReviewBodies/SysAdmin_Modification.feature
@@ -1,27 +1,53 @@
 @UserAdministration @ViewReviewBodies @SysAdminUser @SystemTest
-Feature: User Administration: Manage Review Bodies list and view review bodies profile details   
+Feature: Develop a Tasklist page that displays modifications ready to be assigned   
    
     Background:
 
         # Given I have navigated to the 'Home_Page'
         # When I click the 'Approvals' link on the 'Home_Page'
-        #         Given I have navigated to the 'Search_Modifications_Page'
-        # Then I can see the 'Search_Modifications_Page'
+        # When I click the 'Tasklist' link on the 'Approvals_page'
         # And I capture the page screenshot
-
+        # Then I can see the 'Modifications_Ready_To_Design_Page'
+        # And I capture the page screenshot
+        
         Given I have navigated to the 'System_Administration_Page'
-        And I click the 'Manage_Review_Bodies' link on the 'System_Administration_Page'
+        When I click the 'Manage_Review_Bodies' link on the 'System_Administration_Page'
         Then I can see the 'Manage_Review_Bodies_Page'
 
-    @pagination 
-        Scenario Outline: Verify pagination in Modification Ready to Assign page when user is on the first page and navigate through each page by clicking page number or by by clicking next link
-      
-        And I am on the 'first' page and it should be visually highlighted to indicate the active page the user is on
-        And I capture the page screenshot
-        #And I can see the list is sorted by default in the descending order of the 'Modification Id'
+
+        Scenario Outline: Verify default sorting in the Modification Ready to Assign page
+
+        Given I can see the 'Modifications_Ready_To_Design_Page'
         And the default page size should be twenty
+        # When I see modification records in a tasklist displaying the following columns: Modification ID, Short project title, Modification type, Date submitted, and Days since submission
+        # Must be implemented in Chris story 4090 4091 .. Also similar step in 4016 Tiji
+        
+        # Then the oldest record is displayed at the top of the page
+        # Must be implemented in Chris story 4091
+
+
+        Scenario Outline: Verify pagination in Modification Ready to Assign page when user is on the first page and navigate through each page by clicking page number or by clicking next link
+      
+        When I am on the 'first' page and it should be visually highlighted to indicate the active page the user is on
+        And I capture the page screenshot
         And the 'Next' button will be 'available' to the user
         And the 'Previous' button will be 'not available' to the user
+        And the current page number should be visually highlighted to indicate the active page the user is on
+        And I capture the page screenshot
+        Then I sequentially navigate through each page by '<Navigation_Method>' from first page to verify pagination results, surrounding pages, and ellipses for skipped ranges      
+        And I capture the page screenshot
+        Examples:
+            | Navigation_Method       |
+            | clicking on page number |
+            | clicking on next link   |
+
+
+        Scenario Outline: Verify pagination in Modification Ready to Assign page when user is on the last page and navigate through each page by clicking page number or by clicking Previous link
+      
+        When I am on the 'last' page and it should be visually highlighted to indicate the active page the user is on
+        And I capture the page screenshot
+        And the 'Previous' button will be 'available' to the user
+        And the 'Next' button will be 'not available' to the user
         And the current page number should be visually highlighted to indicate the active page the user is on
         And I capture the page screenshot
         Then I sequentially navigate through each page by '<Navigation_Method>' from first page to verify pagination results, surrounding pages, and ellipses for skipped ranges      
