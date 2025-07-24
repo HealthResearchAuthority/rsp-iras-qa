2c5328c7e95968db4181f10cd2cf0f52048c78d4
Chris-McNeill-PA-HRA <Chris.McNeill_PA@hra.nhs.uk>
2025-07-24 15:19:24 +0100

    Feature/rsp 4090 sort modifications (#78)
    * updating OrgName audit history update
    
    * updating role checkbox data
    
    * updating role checkbox data across all files
    
    * adding updated locator for search bars, removing email input data and scenarios from edit tests, adding unique tags to scenarios with common tags
    
    * adding updates to last logged in date test, to include am/pm verification plus additonal text displayed where relevat
    
    * committing WIP for sorting modifications
    
    * updating modification id sort, to sort by number + adding other sort columns using standard string compare sort
    
    * adding axe accessibility test for modifications search page
    
    * adding screenshots to sys test + adding regression test (note more to be added here as other related tickets are completed and merged)
    
    * adding step that was removed by accident in sorting test
    
    * updating sort assertion step to specify the type of list being assessed, to avoid confusion with tasklist mods
    
    * addig tsconfig file to allow use of latest JS methods such as toSort, added additonal properties to tsconfig to resolve issues that occurred due to this update
    
    * removing ! symbol from capture screenshot as per SonarQube as it has now meaningful impact on the expression

--- src/hooks/CustomFixtures.ts
+++ src/hooks/CustomFixtures.ts
@@ -47,6 +47,7 @@ import SearchAddUserReviewBodyPage from '../pages/IRAS/reviewResearch/userAdmini
 import CheckAddUserReviewBodyPage from '../pages/IRAS/reviewResearch/userAdministration/manageReviewBodies/CheckAddUserReviewBodyPage';
 import AccessDeniedPage from '../pages/IRAS/AccessDeniedPage';
 import RtsPage from '../pages/Common/RtsPage';
+import SearchModificationsPage from '../pages/IRAS/reviewResearch/receiveAmendments/SearchModificationsPage';
 
 type CustomFixtures = {
   commonItemsPage: CommonItemsPage;
@@ -95,6 +96,7 @@ type CustomFixtures = {
   checkAddUserReviewBodyPage: CheckAddUserReviewBodyPage;
   accessDeniedPage: AccessDeniedPage;
   rtsPage: RtsPage;
+  searchModificationsPage: SearchModificationsPage;
   makeAxeBuilder: () => AxeBuilder;
 };
 
@@ -281,6 +283,10 @@ export const test = base.extend<CustomFixtures>({
 
   rtsPage: async ({ page }, use) => {
     await use(new RtsPage(page));
+  },
+
+  searchModificationsPage: async ({ page }, use) => {
+    await use(new SearchModificationsPage(page));
   },
 
   makeAxeBuilder: async ({ page }, use) => {
--- src/steps/Common/CommonSteps.ts
+++ src/steps/Common/CommonSteps.ts
@@ -32,6 +32,7 @@ When(
       manageUsersPage,
       searchAddUserReviewBodyPage,
       myResearchProjectsPage,
+      searchModificationsPage,
     },
     page: string
   ) => {
@@ -68,6 +69,9 @@ When(
         break;
       case 'My_Research_Page':
         await myResearchProjectsPage.assertOnMyResearchProjectsPage();
+        break;
+      case 'Search_Modifications_Page':
+        await searchModificationsPage.assertOnSearchModificationsPage();
         break;
       default:
         throw new Error(`${page} is not a valid option`);
@@ -652,6 +656,7 @@ Given(
       userProfilePage,
       reviewBodyProfilePage,
       myResearchProjectsPage,
+      searchModificationsPage,
     },
     page: string
   ) => {
@@ -688,6 +693,10 @@ Given(
       case 'My_Research_Page':
         await myResearchProjectsPage.goto();
         await myResearchProjectsPage.assertOnMyResearchProjectsPage();
+        break;
+      case 'Search_Modifications_Page':
+        await searchModificationsPage.goto();
+        await searchModificationsPage.assertOnSearchModificationsPage();
         break;
       default:
         throw new Error(`${page} is not a valid option`);
