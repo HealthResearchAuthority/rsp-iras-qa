6537b5b93f407754c475f8dcf2eb361bb23ebdc6
Chris-McNeill-PA-HRA <Chris.McNeill_PA@hra.nhs.uk>
2025-07-30 13:08:36 +0100

    Feature/rsp 4091 sort tasklist (#80)
    * updating OrgName audit history update
    
    * updating role checkbox data
    
    * updating role checkbox data across all files
    
    * adding updated locator for search bars, removing email input data and scenarios from edit tests, adding unique tags to scenarios with common tags
    
    * adding updates to last logged in date test, to include am/pm verification plus additonal text displayed where relevat
    
    * committing WIP for sorting modifications
    
    * updating modification id sort, to sort by number + adding other sort columns using standard string compare sort
    
    * adding axe accessibility test for modifications search page
    
    * adding screenshots to sys test + adding regression test (note more to be added here as other related tickets are completed and merged)
    
    * adding initial work for 4091
    
    * adding latest to 4091
    
    * updating 4091 with latest scenarios + adding sort steps for data and days since submitted
    
    * uncommenting examples for auth
    
    * updatig steps in AXE Accessibilty scenario
    
    * merging 2 separate sort scenario into 1, adding regression test for Mods Ready To Assign, renaming sort system test to fix an error
    
    * addressing alfreds PR comments + removing and editing tags committed by mistake
    
    * adding locator for new table assertion step on search mods
    
    * removing unused step, adding toReversed method to fix sonarQube issue
    
    * placing new table assert step in the correct order in sys test feature file

--- src/steps/Common/CommonSteps.ts
+++ src/steps/Common/CommonSteps.ts
@@ -33,6 +33,7 @@ When(
       searchAddUserReviewBodyPage,
       myResearchProjectsPage,
       searchModificationsPage,
+      modificationsReadyToAssignPage,
     },
     page: string
   ) => {
@@ -72,6 +73,9 @@ When(
         break;
       case 'Search_Modifications_Page':
         await searchModificationsPage.assertOnSearchModificationsPage();
+        break;
+      case 'Modifications_Tasklist_Page':
+        await modificationsReadyToAssignPage.assertOnModificationsReadyToAssignPage();
         break;
       default:
         throw new Error(`${page} is not a valid option`);
@@ -671,6 +675,7 @@ Given(
       reviewBodyProfilePage,
       myResearchProjectsPage,
       searchModificationsPage,
+      modificationsReadyToAssignPage,
     },
     page: string
   ) => {
@@ -711,6 +716,10 @@ Given(
       case 'Search_Modifications_Page':
         await searchModificationsPage.goto();
         await searchModificationsPage.assertOnSearchModificationsPage();
+        break;
+      case 'Modifications_Tasklist_Page':
+        await modificationsReadyToAssignPage.goto();
+        await modificationsReadyToAssignPage.assertOnModificationsReadyToAssignPage();
         break;
       default:
         throw new Error(`${page} is not a valid option`);
