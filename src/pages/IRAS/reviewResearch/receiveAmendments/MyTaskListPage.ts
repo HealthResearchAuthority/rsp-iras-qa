92e3cf3d3b46af0b4eeb80863b4d5d55d806f101
Tiji Manikombel Jose <tiji.manikombel-jose@hra.nhs.uk>
2025-09-12 14:27:12 +0100

    SWR tasklist page updated

--- src/pages/IRAS/reviewResearch/receiveAmendments/MyTaskListPage.ts
+++ src/pages/IRAS/reviewResearch/receiveAmendments/MyTaskListPage.ts
@@ -7,22 +7,13 @@ export default class MyTaskListPage {
   readonly page: Page;
   readonly myTaskListPageTestData: typeof myTaskListPageTestData;
   readonly linkTextData: typeof linkTextData;
-  private _modifications_list_after_search: string[];
   readonly page_heading: Locator;
   readonly page_guidance_text: Locator;
-  readonly iras_id_search_text: Locator;
-  readonly search_box: Locator;
-  readonly search_button_label: Locator;
-  readonly next_button: Locator;
-  readonly search_box_label: Locator;
-  readonly listCell: Locator;
-  readonly tableRows: Locator;
 
   //Initialize Page Objects
   constructor(page: Page) {
     this.page = page;
     this.myTaskListPageTestData = myTaskListPageTestData;
-    this._modifications_list_after_search = [];
 
     //Locators
     this.page_heading = this.page
@@ -31,26 +22,6 @@ export default class MyTaskListPage {
     this.page_guidance_text = this.page
       .getByRole('paragraph')
       .getByText(this.myTaskListPageTestData.My_Task_List_Page.page_guidance_text);
-    this.iras_id_search_text = this.page.getByLabel(
-      this.myTaskListPageTestData.My_Task_List_Page.iras_id_search_box_label,
-      { exact: true }
-    );
-    this.search_box_label = this.page.locator('label[for="SearchQuery"]');
-    this.search_box = this.page.getByTestId('SearchQuery');
-    this.search_button_label = this.page.getByText('Search');
-    this.next_button = this.page.locator('.govuk-pagination__next a');
-    this.tableRows = this.page.getByRole('table').getByRole('row');
-    this.listCell = this.page.getByRole('cell');
-  }
-
-  //Getters & Setters for Private Variables
-
-  async getModificationIdListAfterSearch(): Promise<string[]> {
-    return this._modifications_list_after_search;
-  }
-
-  async setModificationIdListAfterSearch(value: string[]): Promise<void> {
-    this._modifications_list_after_search = value;
   }
 
   //Page Methods
