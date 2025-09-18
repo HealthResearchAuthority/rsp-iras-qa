0c690bbd493e41f2ff49da79b21a359b6d152415
Chris McNeill <chris.mcneill_pa@hra.nhs.uk>
2025-09-18 04:15:42 +0100

    small fix to reference correct page that sort method resides

--- src/steps/IRAS/makeChanges/MyResearchProjectsSteps.ts
+++ src/steps/IRAS/makeChanges/MyResearchProjectsSteps.ts
@@ -28,7 +28,7 @@ Then(
 Then(
   'I can see my research page is sorted by {string} order of the {string}',
   async (
-    { myResearchProjectsPage, modificationsReadyToAssignPage, commonItemsPage },
+    { myResearchProjectsPage, modificationsReceivedCommonPage, commonItemsPage },
     sortDirection: string,
     sortField: string
   ) => {
@@ -54,7 +54,7 @@ Then(
     if (sortField.toLowerCase() == 'iras id') {
       sortedList = await myResearchProjectsPage.sortIrasIdListValues(actualList, sortDirection);
     } else if (sortField.toLowerCase() == 'date created') {
-      sortedList = await modificationsReadyToAssignPage.sortDateSubmittedListValues(actualList, sortDirection);
+      sortedList = await modificationsReceivedCommonPage.sortDateSubmittedListValues(actualList, sortDirection);
     } else if (sortDirection.toLowerCase() == 'ascending') {
       sortedList = [...actualList].sort((a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' }));
     } else {
