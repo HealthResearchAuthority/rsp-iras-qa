88fd4684fb3d69991a690e541f7893ab58819d80
Chris McNeill <chris.mcneill_pa@hra.nhs.uk>
2025-10-14 14:17:54 +0100

    stashing small update to global setup with notes for net steps

--- src/hooks/GlobalSetup.ts
+++ src/hooks/GlobalSetup.ts
@@ -1,5 +1,20 @@
+import { request } from '@playwright/test';
+
 async function globalSetup() {
   process.env.BASE_URL = 'https://fd-rsp-applications-automationtest-uks-e7f6hkg3c5edhxex.a03.azurefd.net/';
+
+  const req = await request.newContext({
+    baseURL: 'https://cmsportal-dev.azurewebsites.net/umbraco/api/',
+    extraHTTPHeaders: {
+      accept: 'application/json',
+    },
+  });
+
+  const getProjectSectionsResponse = await req.get('projectRecordQuestionset/getQuestionSections');
+  console.log(getProjectSectionsResponse.status());
+  // console.log(await getProjectSectionsResponse.body());
+  //write to file
+  //do for project qset labels and Id's
 }
 export default globalSetup;
 export const deviceDSafari = 'Desktop Safari';
