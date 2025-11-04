d9296d0c90986444ebd148137ad064850686739f
vijay-kumar-hra <Vijayakumar.rasappan@hra.nhs.uk>
2025-11-04 12:37:02 +0000

    fixed SonarQube issue

--- src/steps/IRAS/makeChanges/modifications/projectDocuments/ReviewYourDocumentInfomationModificationsSteps.ts
+++ src/steps/IRAS/makeChanges/modifications/projectDocuments/ReviewYourDocumentInfomationModificationsSteps.ts
@@ -266,27 +266,24 @@ Then(
       addDocumentDetailsForSpecificDocumentModificationsPage
         .addDocumentDetailsForSpecificDocumentModificationsPageTestData[datasetDate];
     const noOfDocuments = await reviewYourDocumentInformationModificationsPage.getNumberOfDocuments();
-    for (let i = 0; i < noOfDocuments; i++) {
-      await reviewYourDocumentInformationModificationsPage.sponsor_document_date_change_link.nth(i).click();
-      for (const key in newDocumentDate) {
-        if (Object.hasOwn(newDocumentDate, key)) {
-          await commonItemsPage.fillUIComponent(
-            newDocumentDate,
-            key,
-            addDocumentDetailsForSpecificDocumentModificationsPage
-          );
-        }
+    for (let index = 0; index < noOfDocuments; index++) {
+      await reviewYourDocumentInformationModificationsPage.sponsor_document_date_change_link.nth(index).click();
+      for (const key of Object.keys(newDocumentDate)) {
+        await commonItemsPage.fillUIComponent(
+          newDocumentDate,
+          key,
+          addDocumentDetailsForSpecificDocumentModificationsPage
+        );
       }
       await addDocumentDetailsForSpecificDocumentModificationsPage.save_changes.click();
-      if (updatedDocumentDate?.textContent !== undefined) {
-        const actualDocumentDate = (
-          await reviewYourDocumentInformationModificationsPage.sponsor_document_date_text.textContent()
-        ).trim();
-        const expectedDocumentDate = await updatedDocumentDate.sponsor_document_date_text;
-        expect.soft(actualDocumentDate).toBe(expectedDocumentDate);
-      } else {
+      if (updatedDocumentDate?.textContent === undefined) {
         break;
       }
+      const actualDocumentDate = (
+        await reviewYourDocumentInformationModificationsPage.sponsor_document_date_text.textContent()
+      ).trim();
+      const expectedDocumentDate = updatedDocumentDate.sponsor_document_date_text;
+      expect.soft(actualDocumentDate).toBe(expectedDocumentDate);
     }
   }
 );
