d9f8dbb0fb5fdea06cf1c8a9ffbcf99105aaed61
vijay-kumar-hra <Vijayakumar.rasappan@hra.nhs.uk>
2025-11-24 23:26:44 +0000

    fixed error files

--- src/steps/IRAS/makeChanges/SponsorWorkspacePageSteps.ts
+++ src/steps/IRAS/makeChanges/SponsorWorkspacePageSteps.ts
@@ -18,26 +18,6 @@ Then(
     await expect(sponsorWorkspacePage.mainPageContent.getByText(authorisationsBodyLabel)).toBeVisible();
   }
 );
-import { createBdd } from 'playwright-bdd';
-import { expect, test } from '../../../hooks/CustomFixtures';
-
-const { Then } = createBdd(test);
-
-Then('I can see the sponsor workspace page', async ({ sponsorWorkspacePage }) => {
-  await sponsorWorkspacePage.assertOnSponsorWorkspacePage();
-});
-
-Then(
-  'I can see the ui labels in sponsor workspace page',
-  async ({ sponsorWorkspacePage, checkSetupSponsorOrganisationPage }) => {
-    const authorisationsBodyLabel =
-      sponsorWorkspacePage.sponsorWorkspacePageTestData.Label_Texts.sponsor_workspace_label.replace(
-        '{{sponsor_organisation_name}}',
-        await checkSetupSponsorOrganisationPage.getOrgName()
-      );
-    await expect(sponsorWorkspacePage.mainPageContent.getByText(authorisationsBodyLabel)).toBeVisible();
-  }
-);
 
 Then(
   'I can see the list of modifications received for sponsor approval is sorted by {string} order of the {string}',
