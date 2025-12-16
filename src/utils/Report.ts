21c3fb4e009f91f9f76253e3b9094900e1979471
Tiji Manikombel Jose <tiji.manikombel-jose@hra.nhs.uk>
2025-12-16 17:30:12 +0000

    REmoved known defect tag, updated cucumber reports

--- src/utils/Report.ts
+++ src/utils/Report.ts
@@ -35,7 +35,17 @@ class MyReporter implements Reporter {
       pageTitle: 'Multiple Cucumber HTML reporter',
       reportName: 'Plan and Manage Health and Care Research - Automation Test Report - Playwright',
       displayDuration: true,
-      pageFooter: '<div><p></p></div>',
+      pageFooter: `
+      <div><p></p></div>
+      <style>
+      /* Hide the entire Features card */
+      .x_panel:has(#feature-chart),
+      .col-md-6.col-lg-4.col-xs-12:has(#feature-chart),
+      td.chart:has(#feature-chart) {
+        display: none !important;
+      }
+      </style>
+   `,
       customMetadata: true,
       metadata: [
         { name: 'Device Type', value: `${deviceTypeVal}` },
