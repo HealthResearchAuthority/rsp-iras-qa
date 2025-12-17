2edfb1eb7cd5766b92f6333efe9aa8edb2d69dcb
vijay-kumar-hra <Vijayakumar.rasappan@hra.nhs.uk>
2025-12-17 09:46:45 +0000

    Removed the commented code

--- src/steps/IRAS/makeChanges/modifications/ModificationsCommonSteps.ts
+++ src/steps/IRAS/makeChanges/modifications/ModificationsCommonSteps.ts
@@ -295,10 +295,10 @@ Then(
 Then(
   'I can see the modification send to sponsor is displayed on post approval tab of project overview page with status as {string}',
   async ({ modificationsCommonPage }, statusValue: string) => {
-    // const modificationIDExpected = await modificationsCommonPage.getModificationID();
+    const modificationIDExpected = await modificationsCommonPage.getModificationID();
     const modificationRecord = await modificationsCommonPage.getModificationPostApprovalPage();
-    // const modificationIDActual = modificationRecord.get('modificationIdValue');
-    // expect.soft(modificationIDActual[0]).toBe(modificationIDExpected);
+    const modificationIDActual = modificationRecord.get('modificationIdValue');
+    expect.soft(modificationIDActual[0]).toBe(modificationIDExpected);
     const statusActual = modificationRecord.get('statusValue');
     expect.soft(statusActual[0]).toBe(statusValue);
   }
