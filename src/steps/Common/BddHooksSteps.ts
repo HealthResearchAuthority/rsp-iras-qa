e02fcd43bff6ec19bda12cbe497e0b9d86e30019
Chris McNeill <chris.mcneill_pa@hra.nhs.uk>
2025-12-24 12:27:43 +0000

    adding new user details

--- src/steps/Common/BddHooksSteps.ts
+++ src/steps/Common/BddHooksSteps.ts
@@ -175,6 +175,7 @@ BeforeScenario(
         'Workflow_Coordinator_S',
         'Workflow_Coordinator_W',
         'Sponsor_User',
+        'Sponsor_Org_Admin_User',
       ];
 
       for (const user of users) {
@@ -205,6 +206,7 @@ BeforeScenario(
         '@WorkFlowCoordinatorSco': 'workflow_coordinator_s',
         '@WorkFlowCoordinatorWal': 'workflow_coordinator_w',
         '@SponsorUser': 'sponsor_user',
+        '@SponsorOrgAdminUser': 'sponsor_org_admin_user',
       };
 
       for (const tag in tagToUserMap) {
