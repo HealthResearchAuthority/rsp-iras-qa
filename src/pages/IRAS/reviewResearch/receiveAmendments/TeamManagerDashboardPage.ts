8f4394dea0e69b08ef377086d1ae61260550539c
Tiji-rsp-hra <tiji.manikombel-jose@hra.nhs.uk>
2026-01-22 17:20:36 +0000

    Code changes for system tests fixing backstage Q8.S3-Approvals workspace (#211)
    * REmoved known defect tag, updated cucumber reports
    
    * Removed review ammendments after merging wth main
    
    * expect.soft added
    
    * ST run fixes
    
    * Update manage sponsor organisation with latest changes and remove RTS if not mandatory
    
    * Fixes in the succes header locator
    
    * ST regression for sponsor fixing
    
    * System Test - Sponsor fixing
    
    * Sponsor workspace code relocated
    
    * Added @skip tag for RTS
    
    * Missing step definition fixed
    
    * Removed duplicate feature file name and test title
    
    * Removed duplicates
    
    * Few fixes to avoid automation test roles update
    
    * Approval Search System Tests fixing
    
    * few fixes in the regresssion
    
    * Search projects and modification ST fixes
    
    * Approval Search fixing
    
    * Testdataprep scripst updated with sponsor authorisation changes
    
    * Approvald search modification fix
    
    * Approval Search Fix
    
    * New defect tag added
    
    * ST Fixing Approvals part 2
    
    * resolving merge conflicts
    
    * SPonsor can't authorise few updates after defect fix
    
    * SWR list modifications
    
    * Sponsor without authorise permission
    
    * comented step Then I can see the 'user added' successful message on sponsor organisation user list page code regression
    
    * removing extra tag

--- src/pages/IRAS/reviewResearch/receiveAmendments/TeamManagerDashboardPage.ts
+++ src/pages/IRAS/reviewResearch/receiveAmendments/TeamManagerDashboardPage.ts
@@ -16,6 +16,7 @@ export default class TeamManagerDashboardPage {
   readonly searchFilterResultsData: typeof searchFilterResultsData;
   private _modification_record: string[];
   private _modification_id: string;
+  private _iras_id: string;
   readonly modifications_tasklist_link: Locator;
   readonly short_project_title_column_label: Locator;
   readonly modification_id_column_label: Locator;
@@ -50,6 +51,7 @@ export default class TeamManagerDashboardPage {
     this.searchFilterResultsData = searchFilterResultsData;
     this._modification_record = [];
     this._modification_id = '';
+    this._iras_id = '';
 
     //Locators
     this.modifications_tasklist_link = this.page.locator('.govuk-heading-s govuk-link hra-card-heading__link');
@@ -130,6 +132,14 @@ export default class TeamManagerDashboardPage {
 
   async setModificationId(value: string): Promise<void> {
     this._modification_id = value;
+  }
+
+  async getIrasId(): Promise<string> {
+    return this._iras_id;
+  }
+
+  async setIrasId(value: string): Promise<void> {
+    this._iras_id = value;
   }
 
   //Page Methods
