78d4f232cd66daf491bdcb36afc2aea0ee1bac6d
alfreddcruz-HRA <alfred.dcruz@hra.nhs.uk>
2026-01-28 09:56:35 +0000

    RSP-4888_4908_5312 - Close research (#205)
    * Initial commit
    
    * Merge branch 'main' of https://github.com/HealthResearchAuthority/rsp-iras-qa into feature/rsp-4888-rsp-4908-rsp-5312-close-research
    
    * Feature File Updated
    
    * Added automation scripts for close research
    
    * Adding code for close research
    
    * Updated known defect tags
    
    * Added code for close research
    
    * Added code for modification counter
    
    ---------
    
    Co-authored-by: bakkiya-HRA <bakkiyarajan.balakrishnan@hra.nhs.uk>

--- src/steps/IRAS/makeChanges/ProjectOverviewSteps.ts
+++ src/steps/IRAS/makeChanges/ProjectOverviewSteps.ts
@@ -570,3 +570,39 @@ Then(
     await projectOverviewPage.page.waitForLoadState('domcontentloaded');
   }
 );
+
+Then('I can see project ending section in project overview page', async ({ projectOverviewPage }) => {
+  await expect
+    .soft(
+      projectOverviewPage.page.getByText(
+        projectOverviewPage.projectOverviewPageTestData.Project_Overview_Page.project_ending_section_heading,
+        { exact: true }
+      )
+    )
+    .toBeVisible();
+});
+
+Then(
+  'I validate project closure date displayed in project details page using {string}',
+  async ({ projectOverviewPage, closeProjectPage }, datasetName: string) => {
+    const dataset = closeProjectPage.closeProjectPageTestData[datasetName];
+    let expectedProjectClosureDate: any;
+    if (datasetName === 'Valid_Date_Today') {
+      const todayDate = new Date();
+      expectedProjectClosureDate =
+        todayDate.getDate().toString() +
+        ' ' +
+        todayDate.toLocaleString('en-US', { month: 'long' }) +
+        ' ' +
+        todayDate.getFullYear().toString();
+    } else {
+      expectedProjectClosureDate =
+        dataset.close_project_actual_project_closure_date_day_text +
+        ' ' +
+        dataset.close_project_actual_project_closure_date_month_dropdown +
+        ' ' +
+        dataset.close_project_actual_project_closure_date_year_text;
+    }
+    await expect.soft(projectOverviewPage.actualProjectClosureDateValueLabel).toHaveText(expectedProjectClosureDate);
+  }
+);
