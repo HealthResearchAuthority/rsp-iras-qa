da3119d6b0f02c83eccb3c11cc818320b3e51052
Veerabhadra Rao Batta <veerabhadra-rao.batta@hra.nhs.uk>
2026-02-18 19:34:59 +0000

    Commented Retries in config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ const config: PlaywrightTestConfig = {
   globalTimeout: 10 * 60 * 60 * 1000,
   timeout: 12 * 60 * 1000,
   workers: 1, // to enforce serial execution
-  retries: 1,
+  //retries: 1,
   use: {
     ...browser,
     trace: 'retain-on-failure',
