871b2e61eb445334b8c1f88ee7beb2eec83a4cb0
vijay-kumar-hra <Vijayakumar.rasappan@hra.nhs.uk>
2026-03-03 15:08:42 +0000

    PR comments updated

--- src/steps/IRAS/makeChanges/SponsorWorkspacePageSteps.ts
+++ src/steps/IRAS/makeChanges/SponsorWorkspacePageSteps.ts
@@ -7,22 +7,15 @@ Then('I can see the sponsor workspace page', async ({ sponsorWorkspacePage }) =>
   await sponsorWorkspacePage.assertOnSponsorWorkspacePage();
 });
 
-Then(
-  'I can see the ui labels in sponsor workspace page',
-  async ({ sponsorWorkspacePage, checkSetupSponsorOrganisationPage }) => {
-    const authorisationsBodyLabel =
-      sponsorWorkspacePage.sponsorWorkspacePageTestData.Label_Texts.authorisations_label.replace(
-        '{{sponsor_organisation_name}}',
-        await checkSetupSponsorOrganisationPage.getOrgName()
-      );
-    await expect.soft(sponsorWorkspacePage.mainPageContent.getByText(authorisationsBodyLabel)).toBeVisible();
-    const myOrganisationsLabel = sponsorWorkspacePage.sponsorWorkspacePageTestData.Label_Texts.my_organisations_label;
-    await expect.soft(sponsorWorkspacePage.mainPageContent.getByText(myOrganisationsLabel)).toBeVisible();
-  }
-);
+Then('I can see the ui labels in sponsor workspace page', async ({ sponsorWorkspacePage }) => {
+  const authorisationsBodyLabel = sponsorWorkspacePage.sponsorWorkspacePageTestData.Label_Texts.authorisations_label;
+  await expect.soft(sponsorWorkspacePage.mainPageContent.getByText(authorisationsBodyLabel)).toBeVisible();
+  const myOrganisationsLabel = sponsorWorkspacePage.sponsorWorkspacePageTestData.Label_Texts.my_organisations_label;
+  await expect.soft(sponsorWorkspacePage.mainPageContent.getByText(myOrganisationsLabel)).toBeVisible();
+});
 
 Then(
-  'I click the {string} link on the sponsor workspace page',
+  'I click the {string} link if displayed on the sponsor workspace page',
   async ({ setupNewSponsorOrganisationPage }, datasetName: string) => {
     const dataset =
       setupNewSponsorOrganisationPage.setupNewSponsorOrganisationPageTestData.Setup_New_Sponsor_Organisation[
